677cbe36642949495c629699d8983bc8505cfb3c
Rostislav <Bronydell@Rostislavs-MacBook-Pro.local>
2016-05-28 12:13:24 +0300

    Fix?

--- bot_logic.py
+++ bot_logic.py
@@ -32,7 +32,6 @@ class Chat:
             self.first = ''
         else:
             self.first = message.text
-        addLine('User '+str(message.chat_id)+' writen '+self.first+' on top of image')
         #print('First step:'+self.first)
         bot.sendMessage(self.id, text='Okay! Now, what do you want to see on bottom of image?', reply_markup=telegram.ReplyKeyboardMarkup([['[NONE TEXT]']]))
 
@@ -41,12 +40,10 @@ class Chat:
             self.second = ''
         else:
             self.second = message.text
-        addLine('User '+str(message.chat_id)+' writen '+self.second+' on bottom of image')
         bot.sendMessage(self.id, text='Nice! Send me a picture', reply_markup=telegram.ReplyKeyboardHide())
 
     def thirdStep(self, bot, message):
         if message.photo:
-            addLine('User '+str(message.chat_id)+' posted image')
             bot.sendChatAction(chat_id=message.chat_id, action=telegram.ChatAction.TYPING)
             bot.sendMessage(message.chat_id, 'Hold on. I trying to download image.')
             bot.getFile(message.photo[-1].file_id).download('images/in_' + str(message.chat_id)+'.jpg')
@@ -56,17 +53,10 @@ class Chat:
 
         else:
             self.step = self.step - 1
-            addLine('User '+str(message.chat_id)+' failed with posting')
             bot.sendMessage(message.chat_id, 'Send me a picture, not this!')
 
 
 chats = list()
-
-
-def addLine(lin):
-    with open('log.txt', 'a') as file:
-        from datetime import datetime
-        file.write('('+ str(datetime.now())+') '+lin+'\n')
 
 def start(bot, update):
     uid = update.message.chat_id
@@ -84,7 +74,6 @@ def feedback(bot, update):
     bot.sendMessage(update.message.chat_id,
                     text='Feel free to share your feedback here: https://storebot.me/bot/creatememe_bot'
                          ' or you can write me directly: @bronydell')
-    addLine('User '+ str(update.message.chat_id)+' called feedback option!')
 
 def create(bot, update):
     uid = update.message.chat_id
@@ -93,13 +82,11 @@ def create(bot, update):
     botan.track(botan_token, uid, message_dict, event_name)
     for chat in chats:
         if chat.id == update.message.chat_id:
-            addLine('Removed user from queue with id '+str(update.message.chat_id))
             chats.remove(chat)
             break
     chat = Chat(update.message.chat_id)
     chats.append(chat)
     bot.sendMessage(update.message.chat_id, text='Let\'s start! Now what you what on top of image?', reply_markup=telegram.ReplyKeyboardMarkup([['[NONE TEXT]']]))
-    addLine('Added new user to queue with id '+str(update.message.chat_id))
 
 
 
@@ -118,7 +105,6 @@ def texting(bot, update):
 
 with open('key.config', 'r') as myfile: #You must put key in key.config!!! IMPORTANT!!!!
     key=myfile.read().replace('\n', '')
-addLine('----------START----------')
 
 updater = Updater(key)
 updater.dispatcher.add_handler(CommandHandler('start', start))
